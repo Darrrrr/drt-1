7bc6f3815a9ec4bd20b1bcb7ab18a4300e9b9162
Darrrrr <15849331968@163.com>
2014-04-28 17:31:04 +0800

    ewe
    ee

--- Office/DetailViewController.m
+++ Office/DetailViewController.m
@@ -31,7 +31,8 @@
     // Update the user interface for the detail item.
     
     if (self.detailItem) {
-        self.detailDescriptionLabel.text = [self.detailItem description];
+        self.detailDescriptionLabel.text = [NSString stringWithFormat:@"%@",[_detailItem objectForKey:@"predocontent"]];
+        self.detailTime.text = [NSString stringWithFormat:@"%@",[_detailItem objectForKey:@"predodate"]];
     }
 }
 
